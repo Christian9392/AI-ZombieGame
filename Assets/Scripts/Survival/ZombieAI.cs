eaa2e24c425bcf222af7efb39aa50b0ee1da410e
pj0077 <s3968971@student.rmit.edu.au>
2025-06-11 18:28:22 +1000

    Zombie-health

--- Assets/Scripts/Survival/ZombieAI.cs
+++ Assets/Scripts/Survival/ZombieAI.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZombieAI : MonoBehaviour
 {
-    public Transform player;           
-    public float moveSpeed = 2f;       
-    public float attackRange = 1f;     
+    public Transform player;
+    public float moveSpeed = 2f;
+    public float attackRange = 1f;
+
+    public float maxHealth = 100f;
+    private float currentHealth;
 
     private Rigidbody2D rb;
     private Animator animator;
+    private Slider healthSlider;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>(); 
+        animator = GetComponent<Animator>();
+
+        currentHealth = maxHealth;
+
+        if (player == null)
+        {
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            if (playerObj != null)
+            {
+                player = playerObj.transform;
+            }
+        }
+
+        healthSlider = transform.Find("HealthBar")?.GetComponent<Slider>();
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
     }
 
     void FixedUpdate()
@@ -23,7 +46,6 @@ public class ZombieAI : MonoBehaviour
         float distance = Vector2.Distance(transform.position, player.position);
 
         bool isMoving = distance > attackRange;
-
         animator.SetBool("isMoving", isMoving);
 
         if (isMoving)
@@ -40,5 +62,27 @@ public class ZombieAI : MonoBehaviour
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+        }
+
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log(gameObject.name + " died!");
+        Destroy(gameObject);
     }
 }
