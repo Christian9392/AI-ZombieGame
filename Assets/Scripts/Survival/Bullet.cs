d9e60317c72a9d49763615b00a81ab53f2a58672
pj0077 <s3968971@student.rmit.edu.au>
2025-06-11 19:35:24 +1000

    Player-shoot

---
+++ Assets/Scripts/Survival/Bullet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Zombie"))
+        {
+            other.GetComponent<ZombieAI>()?.TakeDamage(20f);
+            Destroy(gameObject); 
+        }
+    }
+}
---
+++ Assets/Scripts/Survival/PlayerShooting.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;   
+    public Transform firePoint;       
+    public float bulletSpeed = 10f;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = firePoint.up * bulletSpeed;
+
+        animator.SetTrigger("Shoot");
+
+
+    }
+}
