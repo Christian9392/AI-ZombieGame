6f9825e44d4f8a86dea406434b16ceaa1820bc46
pj0077 <s3968971@student.rmit.edu.au>
2025-06-11 13:17:05 +1000

    zombie-animation

--- Assets/Scripts/Survival/ZombieAI.cs
+++ Assets/Scripts/Survival/ZombieAI.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class ZombieAI : MonoBehaviour
 {
-    public Transform player;          
-    public float moveSpeed = 2f;      
-    public float attackRange = 1f;    
+    public Transform player;           
+    public float moveSpeed = 2f;       
+    public float attackRange = 1f;     
 
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); 
     }
 
     void FixedUpdate()
@@ -20,7 +22,11 @@ public class ZombieAI : MonoBehaviour
         Vector2 direction = (player.position - transform.position).normalized;
         float distance = Vector2.Distance(transform.position, player.position);
 
-        if (distance > attackRange)
+        bool isMoving = distance > attackRange;
+
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
         {
             rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
         }
@@ -32,7 +38,7 @@ public class ZombieAI : MonoBehaviour
         if (direction != Vector2.zero)
         {
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);  
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
     }
 }
